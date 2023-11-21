7cd1a2334c2da1526ca7a97b5358d95ec0d91c72
WolfoIsBestWolf <max.haldim@gmail.com>
2023-11-21 14:23:52 +0100

    1.0.0

---
+++ AegisRemovesBarrierDecay/code/AegisRemovesBarrierDecay.cs
@@ -0,0 +1,32 @@
+using BepInEx;
+using RoR2;
+using R2API;
+using System;
+using UnityEngine;
+
+namespace AegisRemovesBarrierDecay
+{
+    [BepInDependency(LanguageAPI.PluginGUID)]
+    [BepInPlugin("com.Wolfo.AegisRemovesBarrierDecay", "AegisRemovesBarrierDecay", "1.0.0")]
+    public class AegisRemovesBarrierDecay : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            On.RoR2.CharacterBody.RecalculateStats += AegisNoDecay;
+            LanguageAPI.Add("ITEM_BARRIERONOVERHEAL_PICKUP", "Healing past full grants you a barrier. Barrier no longer decays.", "en");
+            LanguageAPI.Add("ITEM_BARRIERONOVERHEAL_DESC", "Healing past full grants you a <style=cIsHealing>barrier</style> for <style=cIsHealing>50% <style=cStack>(+50% per stack)</style></style> of the amount you <style=cIsHealing>healed</style>. All <style=cIsHealing>barrier</style> no longer naturally decays.", "en");
+        }
+
+        private void AegisNoDecay(On.RoR2.CharacterBody.orig_RecalculateStats orig, CharacterBody self)
+        {
+            orig(self);
+            if (self.inventory)
+            {
+                if (self.inventory.GetItemCount(RoR2Content.Items.BarrierOnOverHeal) > 0)
+                {
+                    self.barrierDecayRate = 0;
+                }
+            }
+        }
+    }
+}
---
+++ mod-AegisRemovesBarrierDecay/README.md
@@ -0,0 +1,5 @@
+If you have Aegis, your temporary barrier is no longer temporary.
+
+This makes the barrier you get from Aegis actually stick around as opposed to immediately decaying.\
+Also helps any other source of Barrier such as Topaz Brooches.\
+This does not make Aegis itself that strong. You will still need on hit or excessive healing but it makes it more unique.
